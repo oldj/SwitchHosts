80efce46df1c3a52d8cd29be3d4f8469c3adbd76
unknown <jizha.wyj@.taobao.ali.com>
2011-09-15 12:16:00 +0800

    将主面板的界面移到 ui.py 中。

--- src/SwitchHosts.py
+++ src/SwitchHosts.py
@@ -15,6 +15,7 @@ import glob
 import traceback
 import wx
 import libs.common_operations as co
+import libs.ui as ui
 
 VERSION = "0.1.1"
 
@@ -127,7 +128,7 @@ class TaskBarIcon(wx.TaskBarIcon):
         co.switchHost(self, fn)
 
 
-class Frame(wx.Frame):
+class Frame(ui.Frame):
 
     ID_RENAME = wx.NewId()
 
@@ -135,72 +136,9 @@ class Frame(wx.Frame):
         self, parent=None, id=wx.ID_ANY, title="Switch Host!", pos=wx.DefaultPosition,
         size=wx.DefaultSize, style=wx.DEFAULT_FRAME_STYLE
     ):
-        wx.Frame.__init__(self, parent, id, title, pos, size, style)
+        ui.Frame.__init__(self, parent, id, title, pos, size, style, TaskBarIcon=TaskBarIcon)
 
-        self.SetIcon(co.GetMondrianIcon())
-        self.taskbar_icon = TaskBarIcon(self)
         self.Bind(wx.EVT_CLOSE, self.OnClose)
-        self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
-
-        self.m_menubar1 = wx.MenuBar(0)
-        self.m_menu1 = wx.Menu()
-        self.m_menuItem_exit = wx.MenuItem(self.m_menu1, wx.ID_EXIT, u"退出(&X)", wx.EmptyString, wx.ITEM_NORMAL)
-        self.m_menu1.AppendItem(self.m_menuItem_exit)
-
-        self.m_menubar1.Append(self.m_menu1, u"文件(&F)")
-
-        self.m_menu2 = wx.Menu()
-        self.m_menuItem_about = wx.MenuItem(self.m_menu2, wx.ID_ABOUT, u"关于(&A)", wx.EmptyString, wx.ITEM_NORMAL)
-        self.m_menu2.AppendItem(self.m_menuItem_about)
-
-        self.m_menubar1.Append(self.m_menu2, u"帮助(&H)")
-
-        self.SetMenuBar(self.m_menubar1)
-
-        bSizer1 = wx.BoxSizer(wx.VERTICAL)
-
-        self.m_panel1 = wx.Panel(self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-        bSizer4 = wx.BoxSizer(wx.HORIZONTAL)
-
-        bSizer5 = wx.BoxSizer(wx.VERTICAL)
-
-        self.m_list = wx.ListCtrl(self.m_panel1, wx.ID_ANY, wx.DefaultPosition, wx.Size(160, 320),
-                                       wx.LC_REPORT)
-        bSizer5.Add(self.m_list, 0, wx.ALL | wx.EXPAND, 5)
-
-        bSizer4.Add(bSizer5, 0, wx.EXPAND, 5)
-
-        bSizer6 = wx.BoxSizer(wx.VERTICAL)
-
-        self.m_textCtrl_content = wx.TextCtrl(self.m_panel1, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
-                                       wx.TE_MULTILINE)
-        bSizer6.Add(self.m_textCtrl_content, 1, wx.ALL | wx.EXPAND, 5)
-
-        bSizer7 = wx.BoxSizer(wx.HORIZONTAL)
-
-        self.m_panel3 = wx.Panel(self.m_panel1, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL)
-        bSizer7.Add(self.m_panel3, 1, wx.EXPAND | wx.ALL, 5)
-
-        self.m_btn_apply = wx.Button(self.m_panel1, wx.ID_APPLY, u"应用", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer7.Add(self.m_btn_apply, 0, wx.ALL, 5)
-
-        self.m_btn_exit = wx.Button(self.m_panel1, wx.ID_CLOSE, u"关闭", wx.DefaultPosition, wx.DefaultSize, 0)
-        bSizer7.Add(self.m_btn_exit, 0, wx.ALL, 5)
-
-        bSizer6.Add(bSizer7, 0, wx.EXPAND, 5)
-
-        bSizer4.Add(bSizer6, 1, wx.EXPAND, 5)
-
-        self.m_panel1.SetSizer(bSizer4)
-        self.m_panel1.Layout()
-        bSizer4.Fit(self.m_panel1)
-        bSizer1.Add(self.m_panel1, 1, wx.EXPAND | wx.ALL, 0)
-
-        self.SetSizer(bSizer1)
-        self.Layout()
-
-        self.Centre(wx.BOTH)
-
         self.init2()
 
 
