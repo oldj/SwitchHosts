d0318518e7bdcf3d5b0733414c8419d0da70598f
unknown <jizha.wyj@.taobao.ali.com>
2011-09-15 12:07:19 +0800

    将部分函数移到 common_operations.py 中。

--- src/SwitchHosts.py
+++ src/SwitchHosts.py
@@ -14,52 +14,9 @@ import sys
 import glob
 import traceback
 import wx
+import libs.common_operations as co
 
 VERSION = "0.1.1"
-
-
-def GetMondrianData():
-    return "\x89PNG\r\n\x1a\n\x00\x00\x00\rIHDR\x00\x00\x00\x10\x00\x00\x00\
-\x10\x08\x06\x00\x00\x00\x1f\xf3\xffa\x00\x00\x00\x04gAMA\x00\x00\xaf\xc87\
-\x05\x8a\xe9\x00\x00\x00\x19tEXtSoftware\x00Adobe ImageReadyq\xc9e<\x00\x00\
-\x02=IDAT8\xcb\xa5\x93AK\xd4a\x10\xc6\x7f\xbb\xad\x1ad\x07MIwK\xad\xccMK\xcd\
-\x83i\x14\x15D\x10\x94\x1ax0\xc2C\xa7\xbe@t\xc8Kt\xab/\xd0)*\xea\xe45\x12B\x02\
-\xd3J-5w\xcd r\xb5\xd2M7SV$\xd4\xff\xff\x9dy\xdf\x0eF(E`>0\x97\xe1a`\xe67O\xc09\
-\xc7f\x14\xdc\x88\xf9\xe2\xa3\xa6\xa7\xcd\xf7\xcee\xfe\xd7\x80\x96\x87\x8d\xf5\
-\xbeoN\x1a\xcf\xbc[\xdb\x0f\\\xe9\xb8\xf4{\x07\xab\x16\xab\x16\x15\xdbg\xd5\xb6\
-\x11\x0c^\x10#g\xc5Ht[f6\x91\xdc\x08s\x0bs$&\xc7c\xcf\xae\xbd\xa8\x01\x08\x01\
-\xb4V]\xc6\xe1p\xcea\x9dcl\xf6\xe3\xd1\xf7\xc9\xd1.\xb1B\xb4\xf8\x00[3\xb3\x10\
-\xb5\xa8S\xf2r\xf2X^^9|\xecF\xed\xcbW7\x07\x8e\x07\x9dsX\x1c\xd6\xd9_\xa5\xec\
-\xc9\xdbKmI-\xe9\x854\x19\xa1\x0c\x8c\x08\xc6\x1a\x8c\x1a\x8c\n\xc6\x08\xc67\x19\
-\x00!+\x96\x07o\xee\xa2\xa2\x04\xed\x16\xf6\xe7\x97q0r\x88\x9c\xed;\x08\xb9\x0c\
-\xc6\x93\x13\xa4\xe6g\x98N\xcd\x10\xde\x19AEHL\x8c\x8f\x0c\xdc\x8a\xd7\x01\x04\
-\xd6bl\xb9\xdf\x90-F\xaf\x97\x87+\xda\x8eD\xeb\x88%\x86\xe9\x89\xf5\xa8:\xdb\xd4\
-\xd5\xd6\xdbQw\xb5F5d?\r\xde\x8e\x97\xfe\x95\x82\x0b\x82\r8\xe2c1\x96\xbc%\xa2E\
-\xe5\xf8?\x0c^\xda\x03\xc0_\xf2\xf1\xe6\xbcut\x02\xad\xed\xcdN\x8c Fp\xe2(\xc8-d1\
-\xbdHY8\xca\xbe\xa2R&\xa6\xc6\x19\x1e\x8d\x93H$8\x18\xad@\xc42484\x92h\xff\\\r\x102\
-\xbe\xe1D\xf9)\xd4)j\x05\xb1\xca\\\xd6<\xf1\x0fq\x8aw\x95\x10.\x88PX\x10\xc1\x9e\xb6\
-\xa8U,\x16o\xc5\xab\xda}>\xfcz\xea\xc9t]\xd0\xf8\xeb/,*,.-\x92\xfa\xfe\x8d\xe7\xbd\xdd\
-\xcc\xa7\xe7\x11'\x88\x15\xc4)b\x15\x11E\x8c\x1a\x80@\xc3\x9d3N\x8c`|A\xfcUDb\xa4O<m\xf3\
-\xd3\xfe\xea#\xf9\x1a\xcd\xcf\xcf\xa7\xb2\xba\x92\xe4\x97$C\xfdoc3\x9d\xb35\x7fP\xf8\x97\
-\x8a\x1a#\xf5*\xda\xa5b'S\x9d\xb3\xd1\rga\xf2\xf1\xd7~5\xda\xadF+\xd7Q\xd8l\x9c\x7f\x02\
-\x9f\xa4l\xb4#4\xd4~\x00\x00\x00\x00IEND\xaeB`\x82"
-
-
-def GetMondrianBitmap():
-    return wx.BitmapFromImage(GetMondrianImage())
-
-
-def GetMondrianImage():
-    import cStringIO
-
-    stream = cStringIO.StringIO(GetMondrianData())
-    return wx.ImageFromStream(stream)
-
-
-def GetMondrianIcon():
-    icon = wx.EmptyIcon()
-    icon.CopyFromBitmap(GetMondrianBitmap())
-    return icon
 
 
 class TaskBarIcon(wx.TaskBarIcon):
@@ -71,7 +28,7 @@ class TaskBarIcon(wx.TaskBarIcon):
         wx.TaskBarIcon.__init__(self)
         #        super(wx.TaskBarIcon, self).__init__()
         self.frame = frame
-        self.SetIcon(GetMondrianIcon(), "Switch Hosts!")
+        self.SetIcon(co.GetMondrianIcon(), "Switch Hosts!")
         #        self.SetIcon(wx.Icon(name="arrow_switch.png", type=wx.BITMAP_TYPE_PNG), "Switch Hosts!")
         self.Bind(wx.EVT_TASKBAR_LEFT_DCLICK, self.OnTaskBarLeftDClick)
         self.Bind(wx.EVT_MENU, self.OnAbout, id=self.ID_About)
@@ -166,31 +123,8 @@ class TaskBarIcon(wx.TaskBarIcon):
     def switchHost(self, event):
         hosts_id = event.GetId()
         fn = self.hosts[hosts_id]
-        #        print(dir(event))
-        if not os.path.isfile(fn):
-            wx.MessageBox(u"hosts 文件 '%s' 不存在！" % fn, "Error!")
 
-        sys_hosts_fn = getSysHostsPath()
-        try:
-            open(sys_hosts_fn, "wb").write(open(fn, "rb").read())
-            self.current_hosts = fn
-            title = os.path.split(fn)[1]
-            self.SetIcon(GetMondrianIcon(), "Hosts: %s" % title)
-            #            wx.NotificationMessage(
-            #                u"Hosts切换成功！",
-            #                u"hosts 已切换为 %s" % title,
-            #                self.frame).Show()
-            self.notify(u"Hosts 已切换为 %s。" % title)
-
-        except Exception:
-            print(traceback.format_exc())
-            wx.MessageBox(u"hosts 未能成功切换！", "Error!")
-
-#            wx.NotificationMessage(
-#                u"Hosts切换失败！",
-#                u"hosts 未能成功切换！",
-#                self.frame).Show()
-#            self.notify(u"hosts 未能成功切换！")
+        co.switchHost(self, fn)
 
 
 class Frame(wx.Frame):
@@ -203,7 +137,7 @@ class Frame(wx.Frame):
     ):
         wx.Frame.__init__(self, parent, id, title, pos, size, style)
 
-        self.SetIcon(GetMondrianIcon())
+        self.SetIcon(co.GetMondrianIcon())
         self.taskbar_icon = TaskBarIcon(self)
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         self.SetSizeHintsSz(wx.DefaultSize, wx.DefaultSize)
@@ -231,16 +165,16 @@ class Frame(wx.Frame):
         bSizer5 = wx.BoxSizer(wx.VERTICAL)
 
         self.m_list = wx.ListCtrl(self.m_panel1, wx.ID_ANY, wx.DefaultPosition, wx.Size(160, 320),
-                                       wx.LC_REPORT | wx.LC_SORT_ASCENDING)
+                                       wx.LC_REPORT)
         bSizer5.Add(self.m_list, 0, wx.ALL | wx.EXPAND, 5)
 
         bSizer4.Add(bSizer5, 0, wx.EXPAND, 5)
 
         bSizer6 = wx.BoxSizer(wx.VERTICAL)
 
-        self.m_textCtrl1 = wx.TextCtrl(self.m_panel1, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
+        self.m_textCtrl_content = wx.TextCtrl(self.m_panel1, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize,
                                        wx.TE_MULTILINE)
-        bSizer6.Add(self.m_textCtrl1, 1, wx.ALL | wx.EXPAND, 5)
+        bSizer6.Add(self.m_textCtrl_content, 1, wx.ALL | wx.EXPAND, 5)
 
         bSizer7 = wx.BoxSizer(wx.HORIZONTAL)
 
@@ -274,6 +208,7 @@ class Frame(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnExit, id=wx.ID_EXIT)
         self.Bind(wx.EVT_MENU, self.taskbar_icon.OnAbout, id=wx.ID_ABOUT)
         self.Bind(wx.EVT_BUTTON, self.OnHide, id=wx.ID_CLOSE)
+        self.Bind(wx.EVT_BUTTON, self.applyHost, id=wx.ID_APPLY)
 
         hosts_cols = (
             (u"", wx.LIST_AUTOSIZE),
@@ -283,6 +218,7 @@ class Frame(wx.Frame):
             self.m_list.InsertColumn(col, txt)
             self.m_list.SetColumnWidth(col, width)
         self.updateHostsList()
+        self.current_hosts_index = -1
 
         self.hosts_item_menu = wx.Menu()
         self.hosts_item_menu.Append(wx.ID_APPLY, u"切换到当前hosts")
@@ -291,28 +227,41 @@ class Frame(wx.Frame):
         self.hosts_item_menu.AppendSeparator()
         self.hosts_item_menu.Append(wx.ID_DELETE, u"删除")
 
+        self.m_btn_apply.Disable()
+
 
 
     def updateHostsList(self):
         u"""更新 hosts 列表"""
 
-        self.hosts_list_indexes = []
         hosts_list = listLocalHosts()
-        hosts_list = [os.path.split(fn) for fn in hosts_list]
+        hosts_list = [list(os.path.split(fn)) + [fn] for fn in hosts_list]
+        self.hosts_lists = hosts_list
 
-        for idx, (folder, fn) in enumerate(hosts_list):
-            index = self.m_list.InsertStringItem(sys.maxint, "")
+        for idx, (folder, fn, fn2) in enumerate(hosts_list):
+            index = self.m_list.InsertStringItem(sys.maxint, u"√")
             self.m_list.SetStringItem(index, 1, fn)
             self.m_list.SetStringItem(index, 2, folder)
-            self.hosts_list_indexes.append(index)
 
         self.Bind(wx.EVT_LIST_ITEM_RIGHT_CLICK, self.OnHostsItemRClick, self.m_list)
         self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.OnHostsItemBeSelected, self.m_list)
 
 
+    def applyHost(self, event):
+        u"""应用某个 hosts"""
+
+        print self.current_hosts_index
+
+
     def OnHostsItemBeSelected(self, event):
-        
-        print dir(event.GetItem())
+
+        idx = event.GetIndex()
+        fn = self.hosts_lists[idx][2]
+        c = open(fn, "rb").read() if os.path.isfile(fn) else ""
+        self.m_textCtrl_content.Value = c
+
+        self.current_hosts_index = idx
+        self.m_btn_apply.Enable()
 
 
     def OnHostsItemRClick(self, event):
@@ -345,17 +294,6 @@ class Frame(wx.Frame):
     def OnClose(self, event):
         self.Hide()
         return False
-
-
-def getSysHostsPath():
-    u"""取得系统 host 文件的路径"""
-
-    if os.name == "nt":
-        path = "C:\\Windows\\System32\\drivers\\etc\\hosts"
-    else:
-        path = "/etc/hosts"
-
-    return path if os.path.isfile(path) else None
 
 
 def listLocalHosts():
