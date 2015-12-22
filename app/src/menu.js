e2e61118b69e814fe852843da5b70f1cab0d395e
oldj <oldj.wu@gmail.com>
2015-12-23 00:49:54 +0800

    实现 Tray 图标切换功能（对MacGap源码做了少许更改）。

--- app/src/menu.js
+++ app/src/menu.js
@@ -7,6 +7,40 @@
 
 var config = require('./config');
 
+function initTray(app) {
+    var tray = {};
+
+    tray.item = MacGap.StatusItem.create({
+        //title: 'Title',
+        //titleFontSize: 16,
+        image: 'public/images/tray_icon.pdf'
+        //alternateImage: 'public/images/tray_icon2.png'
+    }, function () {
+        //alert('Item clicked');
+    });
+
+
+    tray.updateTrayMenu = function (hosts) {
+        var menu = MacGap.Menu.create('My Menu', 'statusbar');
+        var keys = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+        hosts && hosts.list && hosts.list.map(function (host, idx) {
+            menu.addItem({
+                label: host.title,
+                keys: idx < keys.length ? 'Cmd + ' + keys.substr(idx, 1): '',
+                on: !!host.on,
+                index: idx
+            }, function () {
+                app.switchHost(host);
+            });
+        });
+
+        MacGap.StatusItem.menu = menu;
+    };
+
+    return tray;
+}
+
 function initMenu(app) {
     var menu;
 
@@ -34,3 +68,4 @@ function initMenu(app) {
 }
 
 exports.initMenu = initMenu;
+exports.initTray = initTray;
