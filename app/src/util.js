5ee0ca21681a03b7687e823be58f11f757c6f57e
oldj <oldj.wu@gmail.com>
2015-12-26 22:17:27 +0800

    Import / Export .

--- app/src/util.js
+++ app/src/util.js
@@ -18,15 +18,43 @@ exports.trim = function (s) {
     return (typeof s === 'string' ? s : s.toString()).replace(/^\s+|\s+$/g, '');
 };
 
-exports.copyObj = function (obj) {
-    var new_obj = {};
-    var k;
-    var v;
-    for (k in obj) {
-        if (obj.hasOwnProperty(k)) {
-            v = obj[v];
-            new_obj[k] = v;
-        }
+function isArray(a) {
+    if (typeof Array.isArray == 'function') {
+        return Array.isArray(a);
     }
-    return new_obj;
-};
+    return Object.prototype.toString.call(o) === '[object Array]';
+}
+exports.isArray = isArray;
+
+function copyObj(obj, deep) {
+    var type = typeof obj;
+    if (type == 'string' || type == 'number' || type == 'boolean') return obj;
+
+    // array
+    if (isArray(obj)) {
+        var a = [];
+        var i;
+        for (i = 0; i < obj.length; i++) {
+            a.push(deep ? copyObj(obj[i]) : obj[i]);
+        }
+        return a;
+    }
+
+    if (type == 'object') {
+        if (obj === null) return obj;
+
+        var new_obj = {};
+        var k;
+        var v;
+        for (k in obj) {
+            if (obj.hasOwnProperty(k)) {
+                v = obj[k];
+                new_obj[k] = deep ? copyObj(v) : v;
+            }
+        }
+        return new_obj;
+    }
+
+    return obj;
+}
+exports.copyObj = copyObj;
