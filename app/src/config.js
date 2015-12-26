ea2467e1b925d0ef75ab03cb5f0e466a4aa16393
oldj <oldj.wu@gmail.com>
2015-12-26 22:42:55 +0800

    v -> 3.1.1

--- app/src/config.js
+++ app/src/config.js
@@ -6,7 +6,7 @@
 "use strict";
 
 module.exports = {
-    VERSION: '3.1.0'
+    VERSION: '3.1.1'
     , url_chk_version: 'http://oldj.github.io/SwitchHosts/v.txt'
     , url_homepage: 'http://oldj.github.io/SwitchHosts/'
     , url_feedback: 'https://github.com/oldj/SwitchHosts/issues'
