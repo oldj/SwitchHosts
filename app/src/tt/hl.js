df61250af5b47f6e7184bb850635c18d8e3cebda
oldj <oldj.wu@gmail.com>
2015-12-29 00:30:23 +0800

    调整语法高亮。

--- app/src/tt/hl.js
+++ app/src/tt/hl.js
@@ -1,8 +1,17 @@
-// CodeMirror, copyright (c) by Marijn Haverbeke and others
-// Distributed under an MIT license: http://codemirror.net/LICENSE
+/**
+ * @author oldj
+ * @blog http://oldj.net
+ */
+
+'use strict';
 
 (function (mod) {
-    mod(require("codemirror"));
+    if (typeof exports == "object" && typeof module == "object") // CommonJS
+        mod(require("../../lib/codemirror"));
+    else if (typeof define == "function" && define.amd) // AMD
+        define(["../../lib/codemirror"], mod);
+    else // Plain browser env
+        mod(CodeMirror);
 })(function (CodeMirror) {
     "use strict";
 
@@ -44,6 +53,6 @@
         };
     });
 
-    //CodeMirror.defineMIME('text/x-host', 'host');
+    CodeMirror.defineMIME('text/x-sh', 'shell');
 
 });
