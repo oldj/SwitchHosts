6acbba815750724a7e8932577c5381c9cc4c1e58
oldj <oldj.wu@gmail.com>
2016-05-02 21:18:11 +0800

    使用 <host-list> 组件。

--- app/src/g_test.js
+++ app/src/g_test.js
@@ -18,6 +18,9 @@
 
     MacGap.File = {
         exists: function (fn) {
+            if (fn.indexOf('data.json') != -1) {
+                return false;
+            }
             return true;
         },
         read: function (fn) {
@@ -34,9 +37,9 @@
 255.255.255.255	broadcasthost
 ::1             localhost`;
             } else if (fn.indexOf('data.json') != -1) {
-                return `{"list":[{"content":"# My Hosts\\n","on":false,"title":"My Hosts","_is_show":true,"refresh_interval":0},{"content":"##\\n# Host Database\\n#\\n# localhost is used to configure the loopback interface\\n# when the system is booting.  Do not change this entry.\\n##\\n127.0.0.1\\tlocalhost\\n255.255.255.255\\tbroadcasthost\\n::1             localhost \\n","on":true,"title":"backup","_is_show":true,"refresh_interval":0},{"last_refresh":null,"refresh_interval":0,"where":"local","on":true,"content":"# 测试\\n\\n127.0.0.1 test.com\\n","title":"测试","_is_show":true,"is_editable":true,"_just_switch":0}],"sys":"##\\n# Host Database\\n#\\n# localhost is used to configure the loopback interface\\n# when the system is booting.  Do not change this entry.\\n##\\n127.0.0.1\\tlocalhost\\n255.255.255.255\\tbroadcasthost\\n::1             localhost \\n"}`;
+                return '{}';
             } else if (fn.indexOf('preferences.json') != -1) {
-                return `{"is_dock_icon_hidden":false}`;
+                return '{"is_dock_icon_hidden":false}';
             }
             return '';
         },
