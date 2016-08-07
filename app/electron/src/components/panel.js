73e9480a3238b365bf786e6d5424522742e62935
oldj <oldj.wu@gmail.com>
2016-08-07 22:03:08 +0800

    start to try electron + react.

---
+++ app/electron/src/components/panel.js
@@ -0,0 +1,18 @@
+/**
+ * @author oldj
+ * @blog http://oldj.net
+ */
+
+'use strict';
+
+import React from 'react';
+
+export default class Panel extends React.Component {
+    render() {
+        return (
+            <div>
+                panel
+            </div>
+        );
+    }
+}
