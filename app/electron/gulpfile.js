d75d4ea953338e3a085c1603ba4bff6258baec87
oldj <oldj.wu@gmail.com>
2016-08-29 22:39:57 +0800

    ..

--- app/electron/gulpfile.js
+++ app/electron/gulpfile.js
@@ -8,6 +8,7 @@
 const fs = require('fs');
 const path = require('path');
 const gulp = require('gulp');
+const beautify = require('js-beautify').js_beautify;
 
 gulp.task('update_version', () => {
     let fn = path.join(__dirname, 'src', 'version.js');
@@ -17,7 +18,15 @@ gulp.task('update_version', () => {
     console.log(`version -> ${version.join('.')}`);
 
     let cnt = `exports.version = ${JSON.stringify(version)};`
-    fs.writeFile(fn, cnt, 'utf-8');
+    fs.writeFileSync(fn, cnt, 'utf-8');
+
+    // update package.json
+    fn = './package.json';
+    cnt = fs.readFileSync(fn);
+    let d = JSON.parse(cnt);
+    d.version = version.slice(0, 3).join('.');
+    cnt = beautify(JSON.stringify(d), {indent_size: 2});
+    fs.writeFileSync(fn, cnt, 'utf-8');
 });
 
 gulp.task('default', () => {
--- app/electron/src/version.js
+++ app/electron/src/version.js
@@ -1,1 +1,1 @@
-exports.version = [3,2,0,4108];
+exports.version = [3,2,0,4119];
