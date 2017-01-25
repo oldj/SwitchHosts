eb9bb0acf611ace58d2e69f02430d6a07f821717
oldj <oldj.wu@gmail.com>
2017-01-25 11:08:50 +0800

    启动时只允许单实例。 #130 #142

--- app/main.js
+++ app/main.js
@@ -81,6 +81,21 @@ function createWindow() {
     require('./bg/events').init(app, contents);
 }
 
+const should_quit = app.makeSingleInstance((commandLine, workingDirectory) => {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) {
+            mainWindow.restore();
+        }
+        mainWindow.show();
+        // mainWindow.focus();
+    }
+});
+
+if (should_quit) {
+    app.quit();
+}
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
--- app/version.js
+++ app/version.js
@@ -1,1 +1,1 @@
-exports.version = [3,2,2,4246];
+exports.version = [3,2,2,4247];
