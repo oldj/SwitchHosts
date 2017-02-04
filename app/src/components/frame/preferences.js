91cc2f80ecaf6632d816560ecd0ea9e47fb3804a
oldj <oldj.wu@gmail.com>
2017-02-04 17:02:36 +0800

    - 在「选项」面板中显示当前版本号 #145  - 在系统托盘菜单中增加「检查更新」菜单项

--- app/src/components/frame/preferences.js
+++ app/src/components/frame/preferences.js
@@ -10,6 +10,7 @@ import Frame from './frame';
 // import classnames from 'classnames';
 import './preferences.less';
 import lang from '../../lang';
+const current_version = require('../../../version').version;
 
 const AUTO_LAUNCH = 'auto_launch';
 
@@ -197,6 +198,7 @@ export default class PreferencesPrompt extends React.Component {
                 {/*<div className="title">{SH_Agent.lang.host_title}</div>*/}
                 {/*<div className="cnt">*/}
                 {/*</div>*/}
+                <div className="current-version">v{current_version.join('.')}</div>
                 {this.prefLanguage()}
                 {this.prefChoiceMode()}
                 {this.prefAfterCmd()}
