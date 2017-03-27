5939127cf158b8891937d6f705505874bdf59751
oldj <oldj.wu@gmail.com>
2017-03-27 23:03:49 +0800

    调整脚本。

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Main',
+      component: Main
     }
   ]
 })
