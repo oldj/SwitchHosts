2886630ef483d05ff605ec4043e39dbbe58316b0
oldj <oldj.wu@gmail.com>
2017-04-01 15:47:27 +0800

    迁移检查更新菜单。

--- app/configs.js
+++ app/configs.js
@@ -5,6 +5,11 @@
 
 'use strict'
 
+const m_ver = require('./version').version
+
 module.exports = {
-  PORT: 50761
+  PORT: 50761,
+  version: m_ver.slice(0, 3).join('.'),
+  version_full: m_ver.join('.'),
+  url_download: 'https://github.com/oldj/SwitchHosts/releases'
 }
