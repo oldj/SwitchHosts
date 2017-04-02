c1e0d47b50cba89bc82321135ed1dbb59038495e
oldj <oldj.wu@gmail.com>
2017-04-02 20:29:52 +0800

    - http 接口只允许来自本机（127.0.0.1）的访问，增加安全性 - 修复 alfred 单选模式的问题

---
+++ app/server/http/api/toggle.js
@@ -0,0 +1,49 @@
+/**
+ * @author oldj
+ * @blog http://oldj.net
+ */
+
+'use strict'
+
+const getUserHosts = require('../../actions/getUserHosts')
+const saveHosts = require('../../actions/saveHosts')
+const getPref = require('../../actions/getPref')
+const svr = require('../../svr')
+
+module.exports = (req, res) => {
+  let id = req.param('id')
+  let is_single
+
+  getPref()
+    .then(pref => {
+      is_single = pref.choice_mode === 'single'
+    })
+    .then(() => getUserHosts())
+    .then(list => {
+      let item = list.find(i => i.id === id)
+      if (!item) {
+        res.end('not-found:' + id)
+        return
+      }
+
+      item.on = !item.on
+
+      if (item.on && is_single) {
+        // 单选模式
+        list.map(i => {
+          if (i.id !== id) {
+            i.on = false
+          }
+        })
+      }
+
+      saveHosts(svr, list)
+        .then(() => {
+          svr.broadcast('reload')
+          res.end('toggle:' + id)
+        })
+    })
+    .catch(e => {
+      res.end(e.toString())
+    })
+}
