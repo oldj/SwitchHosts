cf2376b28c633f6d2180b7b71a63216f198c6444
oldj <oldj.wu@gmail.com>
2017-04-03 14:22:49 +0800

    修复手动刷新时编辑器中的内容没有自动同步更新的问题。

--- ui/events/list_updated.js
+++ ui/events/list_updated.js
@@ -15,6 +15,11 @@ module.exports = (app, new_list, hosts = null) => {
 
       if (hosts) {
         state.current = hosts
+      } else if (app.state.current) {
+        let c = new_list.find(i => i.id === app.state.current.id)
+        if (c) {
+          state.current = c
+        }
       }
       let current = app.state.current
       if (current.is_sys) {
