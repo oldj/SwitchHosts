272f419f020c967c72cbfd63465e61c52968beed
oldj <oldj.wu@gmail.com>
2017-05-16 19:02:50 +0800

    mv legacy code to branch with-legacy.

--- legacy/v1/libs/nix_notify.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import pynotify
-
-def showNotify(title, msg):
-    pynotify.Notification(title, msg).show()
-
-if __name__ == "__main__":
-    showNotify(u"标题", u"内容")
--- legacy/v2/src/libs/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-#
-# author: oldj
-# blog: http://oldj.net
-# email: oldj.wu@gmail.com
-#
