a502a0a340a05ae930618d66a6a630884b28a34f
oldj <oldj.wu@gmail.com>
2017-12-13 17:44:36 +0800

    直接重启网络侵略性太大，恢复为较温和的方式。

--- app/server/applyAfter_Unix.js
+++ app/server/applyAfter_Unix.js
@@ -13,7 +13,22 @@ const platform = process.platform
 
 function forMac (sudo_pswd, callback) {
   let cmd_fn = path.join(work_path, '_restart_net.sh')
-  let cmd = `for i in \`ifconfig | grep ": flags=" | sed -E 's/(.*): .*/\\1/g'\`;do sudo ifconfig $i down;sudo ifconfig $i up;done`
+  //let cmd = `for i in \`ifconfig | grep ": flags=" | sed -E 's/(.*): .*/\\1/g'\`;do sudo ifconfig $i down;sudo ifconfig $i up;done`
+  let cmd = `
+p1=/System/Library/LaunchDaemons/com.apple.mDNSResponder.plist
+if [ -f $p1 ]; then
+    launchctl unload -w $p1
+    launchctl load -w $p1
+fi
+
+p2=/System/Library/LaunchDaemons/com.apple.discoveryd.plist
+if [ -f p2 ]; then
+    launchctl unload -w $p2
+    launchctl load -w $p2
+fi
+
+killall -HUP mDNSResponder
+`
 
   fs.writeFileSync(cmd_fn, cmd, 'utf-8')
 
