99fbaf953c5123d5c42ea0f43078a70b18d14ee5
oldj <oldj.wu@gmail.com>
2018-04-08 19:19:51 +0800

    Add custom user-agent in HTTP request header. #288

--- app/server/actions/getUrl.js
+++ app/server/actions/getUrl.js
@@ -6,14 +6,20 @@
 'use strict'
 
 const request = require('request')
+const {version} = require('../../version')
 //process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
 
 module.exports = (svr, url) => {
   return new Promise((resolve, reject) => {
-    request({
+    let options = {
       url,
+      headers: {
+        'User-Agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36 SwitchHosts/${version.join('.')}`
+      },
       rejectUnauthorized: false
-    }, (err, res, body) => {
+    }
+
+    request(options, (err, res, body) => {
       if (err) {
         console.log(err)
         reject(err)
