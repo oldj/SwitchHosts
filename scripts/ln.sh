afaf0421ff3fc51a9bfce5685721371319cab783
oldj <oldj.wu@gmail.com>
2019-07-27 17:02:42 +0800

    use new logo.

--- scripts/ln.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# this script will be run at folder `../`
-# the working dir will be `../`
-
-source=$(pwd)/app-ui/styles/iconfont
-target=$(pwd)/node_modules/antd/lib/style/core/iconfont
-
-rm -f ${target} && ln -s ${source} ${target}
