df844a33bb778414e65570b165c16ccbc3dcb654
oldj <oldj.wu@gmail.com>
2019-11-18 13:12:48 +0800

    Fixed an issue about deleting child node. #401

--- app-ui/events/del_hosts.js
+++ app-ui/events/del_hosts.js
@@ -6,21 +6,25 @@
 'use strict'
 
 import Agent from '../Agent'
+import treeFunc from '../../app/libs/treeFunc'
 
 module.exports = (app, hosts) => {
   let list = app.state.list
-  let idx = list.findIndex(item => item.id === hosts.id)
-  if (idx === -1) {
-    return
-  }
+  let neighbors = treeFunc.getNeighbors(list, hosts.id)
+  list = treeFunc.removeItemFromTreeById(list, hosts.id)
 
-  list.splice(idx, 1)
+  //let idx = list.findIndex(item => item.id === hosts.id)
+  //if (idx === -1) {
+  //  return
+  //}
+  //
+  //list.splice(idx, 1)
 
   Agent.pact('saveHosts', list)
     .then(list => {
       app.setState({list}, () => {
         // 选中下一个 hosts
-        let next_hosts = list[idx] || list[idx - 1] || null
+        let next_hosts = neighbors.next || neighbors.previous || null
         if (next_hosts) {
           app.setState({current: next_hosts})
         }
