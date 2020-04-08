735ffd12b9ef969f9c3e4baec85af8daea12cd56
oldj <oldj.wu@gmail.com>
2020-04-08 16:49:54 +0800

    update themes.

--- app/version.js
+++ app/version.js
@@ -1,1 +1,1 @@
-module.exports = [3, 5, 5, 5529]
+module.exports = [3, 5, 5, 5533]
---
+++ scripts/less.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# this script will be run at folder `../`
+# the working dir will be `../`
+
+lessc --js --clean-css app-ui/styles/themes/light/index.less app/ui/theme-light.css
+lessc --js --clean-css app-ui/styles/themes/dark/index.less app/ui/theme-dark.css
