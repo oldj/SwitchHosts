1407beec138a9151874093d64c34b213bdf16dcf
cuikangyi <cuikangyi@gmail.com>
2020-06-12 10:52:44 +0800

    add portable mode support

--- app/server/paths.js
+++ app/server/paths.js
@@ -5,6 +5,7 @@
 
 'use strict'
 
+const fs = require('fs')
 const path = require('path')
 const io = require('./io')
 const platform = process.platform
@@ -12,8 +13,32 @@ const platform = process.platform
 // Windows 系统有可能不安装在 C 盘
 const sys_hosts_path = platform === 'win32' ? `${process.env.windir || 'C:\\WINDOWS'}\\system32\\drivers\\etc\\hosts` : '/etc/hosts'
 
+const appRoot = path.dirname(__dirname)
+
+function getApplicationPath() {
+  if (process.platform === 'darwin') {
+    return path.dirname(path.dirname(path.dirname(appRoot)))
+  }
+
+  return path.dirname(path.dirname(appRoot))
+}
+
+function getPortableDataPath() {
+  if (process.env['SWITCHHOSTS_PORTABLE']) {
+    return process.env['SWITCHHOSTS_PORTABLE'];
+  }
+
+  if (process.platform === 'win32' || process.platform === 'linux') {
+    return path.join(getApplicationPath(), 'data');
+  }
+
+  return path.join(path.dirname(getApplicationPath()), '.SwitchHosts-Data');
+}
+const portableDataPath = getPortableDataPath()
+const isPortable = fs.existsSync(portableDataPath)
+
 const home_path = io.getUserHome()
-const work_path = path.join(home_path, '.SwitchHosts')
+const work_path = isPortable ? portableDataPath : path.join(home_path, '.SwitchHosts')
 const data_path = path.join(work_path, 'data.json')
 const preference_path = path.join(work_path, 'preferences.json')
 
