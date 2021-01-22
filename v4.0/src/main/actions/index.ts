df556ac049a5acba5f1900187734319f777ef80b
oldj <oldj.wu@gmail.com>
2021-01-22 22:40:34 +0800

    update broadcast.

--- v4.0/src/main/actions/index.ts
+++ v4.0/src/main/actions/index.ts
@@ -4,12 +4,12 @@
  * @homepage: https://oldj.net
  */
 
-export { default as ping } from './ping'
 export { default as configGet } from './configGet'
 export { default as configSet } from './configSet'
 export { default as getDataFolder } from './getDataFolder'
+export { default as getSystemHosts } from './getSystemHostsPath'
 export { default as localDataRead } from './localDataRead'
 export { default as localDataWrite } from './localDataWrite'
-export { default as getSystemHosts } from './getSystemHostsPath'
+export { default as ping } from './ping'
 export { default as systemHostsRead } from './systemHostsRead'
 export { default as systemHostsWrite } from './systemHostsWrite'
--- v4.0/src/main/agent.ts
+++ v4.0/src/main/agent.ts
@@ -4,40 +4,8 @@
  * @homepage: https://oldj.net
  */
 
-import { ActionData } from '@main/types'
 import { ipcMain } from 'electron'
-import * as actions from '@main/actions'
 
-function sendBack(sender: any, event_name: string, data: [any] | [any, any]) {
-  try {
-    sender.send(event_name, ...data)
-  } catch (e) {
-    console.error(e)
-  }
+export const broadcast = async (event: string, ...args: any[]) => {
+  ipcMain.emit('x_broadcast', null, { event, args })
 }
-
-ipcMain.on('x_action', async (e, action_data: ActionData) => {
-  let sender = e.sender
-  let { action, data, callback } = action_data
-
-  let fn = actions[action]
-  if (typeof fn === 'function') {
-    let params = data || []
-    if (!Array.isArray(params)) {
-      params = [params]
-    }
-
-    try {
-      // @ts-ignore
-      let v = await fn(...params)
-      sendBack(sender, callback, [null, v])
-    } catch (e) {
-      console.error(e)
-      sendBack(sender, callback, [e])
-    }
-  } else {
-    let e = `unknow action [${action}].`
-    console.error(e)
-    sendBack(sender, callback, [e])
-  }
-})
