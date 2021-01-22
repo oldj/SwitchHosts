313fb7d13c0876ef7a2a36a74c4d1cda548408e0
oldj <oldj.wu@gmail.com>
2021-01-22 23:35:58 +0800

    add notification.

--- v4.0/src/common/i18n/languages/zh.ts
+++ v4.0/src/common/i18n/languages/zh.ts
@@ -8,10 +8,14 @@ import { LanguageDict } from '@root/common/i18n/types'
 
 const lang: LanguageDict = {
   _name: '中文',
+  fail: '操作失败',
+  hosts_updated: 'Hosts 文件已更新。',
   line: '行',
   lines: '行',
   loading: '加载中...',
+  no_access_to_hosts: '没有写入 Hosts 文件的权限。',
   read_only: '只读',
+  success: '操作成功',
   system_hosts: '系统 Hosts',
   test: '测试',
   untitled: '未命名',
