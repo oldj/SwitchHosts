82b095f7379d14cee10be7ef6dccdb080d962249
oldj <oldj.wu@gmail.com>
2021-02-15 22:40:17 +0800

    import material-ui.

--- v4.0/src/common/i18n/languages/en.ts
+++ v4.0/src/common/i18n/languages/en.ts
@@ -6,12 +6,15 @@
 
 export default {
   _name: 'English',
+  btn_cancel: 'Cancel',
+  btn_ok: 'OK',
   fail: 'Fail',
   hosts_updated: 'The Hosts file has been updated.',
   line: 'line',
   lines: 'lines',
   loading: 'Loading...',
-  migrate_confirm: 'Do you want to migrate the old data?',
+  migrate_data: 'Migrate data',
+  migrate_confirm: 'SwitchHosts! v4.0 uses a new data storage format, do you want to migrate old data to the new format?',
   no_access_to_hosts: 'No permission to write to the Hosts file.',
   read_only: 'Read only',
   success: 'Success',
