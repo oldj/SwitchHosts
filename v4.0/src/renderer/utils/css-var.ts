d8d689c52aa1603b019f281059eea1cf0d3ca2d0
oldj <oldj.wu@gmail.com>
2021-02-15 23:13:21 +0800

    get css variables.

---
+++ v4.0/src/renderer/utils/css-var.ts
@@ -0,0 +1,9 @@
+/**
+ * css-var
+ * @author: oldj
+ * @homepage: https://oldj.net
+ */
+
+export const getCssVar = (name: string): string => {
+  return getComputedStyle(document.documentElement).getPropertyValue(name).trim()
+}
