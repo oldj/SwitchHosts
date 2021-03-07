82badd661d660e5c391bf5b0716943c742de63eb
oldj <oldj.wu@gmail.com>
2021-03-07 16:21:52 +0800

    try to write hosts to system with sudo password.

---
+++ v4.0/src/main/libs/safePSWD.ts
@@ -0,0 +1,13 @@
+/**
+ * safe-pswd
+ * @author oldj
+ * @blog https://oldj.net
+ */
+
+
+export default (pswd: string): string => {
+  return pswd
+    .replace(/\\/g, '\\\\')
+    //.replace(/'/g, "\\''")
+    .replace(/'/g, '\\x27')
+}
