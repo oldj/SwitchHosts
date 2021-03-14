8b5baa49293df61c19bcca13730ce1bd5a24c85b
oldj <oldj.wu@gmail.com>
2021-03-14 17:19:49 +0800

    add chakra ui.

--- v4.0/src/renderer/components/EditHostsInfo.tsx
+++ v4.0/src/renderer/components/EditHostsInfo.tsx
@@ -11,12 +11,28 @@ import {
   CheckSquareOutlined,
   DeleteOutlined,
 } from '@ant-design/icons'
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Radio,
+  RadioGroup,
+  Stack,
+  Input,
+  Select,
+} from '@chakra-ui/react'
 import ItemIcon from '@renderer/components/ItemIcon'
 import { actions, agent } from '@renderer/core/agent'
 import useOnBroadcast from '@renderer/core/useOnBroadcast'
 import { HostsWhereType, IHostsListObject } from '@root/common/data'
 import * as hostsFn from '@root/common/hostsFn'
-import { Button, Input, message, Modal, Radio, Select, Transfer } from 'antd'
+import { message } from 'antd'
+import Transfer from '@renderer/components/Transfer'
 import lodash from 'lodash'
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
@@ -112,7 +128,7 @@ const EditHostsInfo = () => {
           <div className={styles.label}>URL</div>
           <div>
             <Input
-              value={hosts?.url}
+              value={hosts?.url || ''}
               onChange={e => onUpdate({ url: e.target.value })}
               placeholder={lang.url_placeholder}
             />
@@ -124,16 +140,16 @@ const EditHostsInfo = () => {
           <div>
             <Select
               value={hosts?.refresh_interval || 0}
-              onChange={refresh_interval => onUpdate({ refresh_interval })}
+              onChange={e => onUpdate({ refresh_interval: parseInt(e.target.value) || 0 })}
               style={{ minWidth: 120 }}
             >
-              <Select.Option value={0}>{lang.never}</Select.Option>
-              <Select.Option value={60}>1 {lang.minute}</Select.Option>
-              <Select.Option value={60 * 5}>5 {lang.minutes}</Select.Option>
-              <Select.Option value={60 * 15}>15 {lang.minutes}</Select.Option>
-              <Select.Option value={60 * 60}>1 {lang.hour}</Select.Option>
-              <Select.Option value={60 * 60 * 24}>24 {lang.hours}</Select.Option>
-              <Select.Option value={60 * 60 * 24 * 7}>7 {lang.days}</Select.Option>
+              <option value={0}>{lang.never}</option>
+              <option value={60}>1 {lang.minute}</option>
+              <option value={60 * 5}>5 {lang.minutes}</option>
+              <option value={60 * 15}>15 {lang.minutes}</option>
+              <option value={60 * 60}>1 {lang.hour}</option>
+              <option value={60 * 60 * 24}>24 {lang.hours}</option>
+              <option value={60 * 60 * 24 * 7}>7 {lang.days}</option>
             </Select>
           </div>
           {is_add ? null : (
@@ -141,8 +157,7 @@ const EditHostsInfo = () => {
               <span>{lang.last_refresh}{hosts?.last_refresh || 'N/A'}</span>
               <Button
                 size="small"
-                type="link"
-                loading={is_refreshing}
+                variant="ghost"
                 disabled={is_refreshing}
                 onClick={() => {
                   if (!hosts) return
@@ -203,8 +218,6 @@ const EditHostsInfo = () => {
         <Transfer
           dataSource={source_list}
           targetKeys={target_keys}
-          listStyle={{ width: '100%' }}
-          oneWay={true}
           render={renderTransferItem}
           onChange={(next_target_keys) => {
             onUpdate({ include: next_target_keys })
@@ -219,15 +232,15 @@ const EditHostsInfo = () => {
       <div className={styles.ln}>
         <div className={styles.label}>{lang.choice_mode}</div>
         <div>
-          <Radio.Group
-            value={hosts?.folder_mode || 0}
-            onChange={e => onUpdate({ folder_mode: e.target.value })}
-          >
-            <Radio.Button value={0}><BorderOuterOutlined/> {lang.choice_mode_default}</Radio.Button>
-            <Radio.Button value={1}><CheckCircleOutlined/> {lang.choice_mode_single}</Radio.Button>
-            <Radio.Button value={2}><CheckSquareOutlined/> {lang.choice_mode_multiple}
-            </Radio.Button>
-          </Radio.Group>
+          {/*<Radio.Group*/}
+          {/*  value={hosts?.folder_mode || 0}*/}
+          {/*  onChange={e => onUpdate({ folder_mode: e.target.value })}*/}
+          {/*>*/}
+          {/*  <Radio.Button value={0}><BorderOuterOutlined/> {lang.choice_mode_default}</Radio.Button>*/}
+          {/*  <Radio.Button value={1}><CheckCircleOutlined/> {lang.choice_mode_single}</Radio.Button>*/}
+          {/*  <Radio.Button value={2}><CheckSquareOutlined/> {lang.choice_mode_multiple}*/}
+          {/*  </Radio.Button>*/}
+          {/*</Radio.Group>*/}
         </div>
       </div>
     )
@@ -235,67 +248,78 @@ const EditHostsInfo = () => {
 
   const wheres: HostsWhereType[] = [ 'local', 'remote', 'group', 'folder' ]
 
-  const footer_buttons: React.ReactElement[] = [
-    <Button key="cancel" onClick={onCancel}>{lang.btn_cancel}</Button>,
-    <Button key="ok" onClick={onSave} type="primary">{lang.btn_ok}</Button>,
-  ]
-  if (!is_add) {
-    footer_buttons.unshift(
-      <Button
-        key="delete"
-        icon={<DeleteOutlined/>}
-        danger
-        style={{ float: 'left' }}
-        disabled={!hosts}
-        onClick={() => {
-          if (hosts) {
-            agent.broadcast('move_to_trashcan', hosts.id)
-            onCancel()
-          }
-        }}
-      >{lang.move_to_trashcan}</Button>,
-    )
-  }
+  const footer_buttons = (
+    <>
+      {
+        is_add ? null : (
+          <Button
+            leftIcon={<DeleteOutlined/>}
+            mr={3}
+            variant="outline"
+            disabled={!hosts}
+            colorScheme="pink"
+            onClick={() => {
+              if (hosts) {
+                agent.broadcast('move_to_trashcan', hosts.id)
+                onCancel()
+              }
+            }}
+          >{lang.move_to_trashcan}</Button>
+        )
+      }
+      <Button onClick={onCancel} mr={3}>{lang.btn_cancel}</Button>
+      <Button onClick={onSave} colorScheme="blue">{lang.btn_ok}</Button>
+    </>
+  )
 
   return (
     <Modal
-      title={is_add ? lang.hosts_add : lang.hosts_edit}
-      visible={is_show}
-      okText={lang.btn_ok}
-      cancelText={lang.btn_cancel}
-      onCancel={onCancel}
-      onOk={onSave}
-      footer={footer_buttons}
+      isOpen={is_show}
+      onClose={onCancel}
+      closeOnOverlayClick={false}
     >
-      <div className={styles.ln}>
-        <div className={styles.label}>{lang.hosts_type}</div>
-        <div>
-          <Radio.Group
-            disabled={!is_add}
-            value={hosts?.where || 'local'}
-            onChange={e => onUpdate({ where: e.target.value })}
-          >
-            {
-              wheres.map(where => (
-                <Radio.Button value={where} key={where}>
-                  <ItemIcon where={where}/> {lang[where]}
-                </Radio.Button>
-              ))
-            }
-          </Radio.Group>
-        </div>
-      </div>
+      <ModalOverlay/>
+      <ModalContent>
+        <ModalHeader>{is_add ? lang.hosts_add : lang.hosts_edit}</ModalHeader>
+        <ModalCloseButton/>
+        <ModalBody pb={6}>
+          <div className={styles.ln}>
+            <div className={styles.label}>{lang.hosts_type}</div>
+            <div>
+              <RadioGroup
+                onChange={(where: HostsWhereType) => onUpdate({ where })}
+                value={hosts?.where || 'local'}
+              >
+                <Stack direction="row">
+                  {
+                    wheres.map(where => (
+                      <Radio value={where} key={where} isDisabled={!is_add}>
+                        <ItemIcon where={where}/> {lang[where]}
+                      </Radio>
+                    ))
+                  }
+                </Stack>
+              </RadioGroup>
+            </div>
+          </div>
 
-      <div className={styles.ln}>
-        <div className={styles.label}>{lang.hosts_title}</div>
-        <div>
-          <Input value={hosts?.title} onChange={e => onUpdate({ title: e.target.value })}/>
-        </div>
-      </div>
+          <div className={styles.ln}>
+            <div className={styles.label}>{lang.hosts_title}</div>
+            <div>
+              <Input value={hosts?.title || ''}
+                     onChange={e => onUpdate({ title: e.target.value })}/>
+            </div>
+          </div>
 
-      {hosts?.where === 'remote' ? forRemote() : null}
-      {hosts?.where === 'group' ? forGroup() : null}
-      {hosts?.where === 'folder' ? forFolder() : null}
+          {hosts?.where === 'remote' ? forRemote() : null}
+          {hosts?.where === 'group' ? forGroup() : null}
+          {hosts?.where === 'folder' ? forFolder() : null}
+        </ModalBody>
+
+        <ModalFooter>
+          {footer_buttons}
+        </ModalFooter>
+      </ModalContent>
     </Modal>
   )
 }
---
+++ v4.0/src/renderer/components/Transfer.tsx
@@ -0,0 +1,140 @@
+/**
+ * Transfer
+ * @author: oldj
+ * @homepage: https://oldj.net
+ */
+
+import { useModel } from '@@/plugin-model/useModel'
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+import { Box, Center, Grid, IconButton, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import clsx from 'clsx'
+import styles from './Transfer.less'
+
+type IdType = string
+
+interface ITransferSourceObject {
+  id: IdType;
+
+  [key: string]: any;
+}
+
+interface IListProps {
+  data: ITransferSourceObject[];
+  selected_keys: IdType[];
+  setSelectedKeys: (ids: IdType[]) => void;
+}
+
+interface Props {
+  dataSource: ITransferSourceObject[];
+  targetKeys: IdType[];
+  render?: (obj: ITransferSourceObject) => React.ReactElement;
+  onChange?: (next_target_keys: IdType[]) => void;
+}
+
+const Transfer = (props: Props) => {
+  const { dataSource, targetKeys, render, onChange } = props
+  const { lang } = useModel('useI18n')
+  const [ right_keys, setRightKeys ] = useState<IdType[]>(targetKeys)
+  const [ left_selectd_keys, setLeftSelectedKeys ] = useState<IdType[]>([])
+  const [ right_selectd_keys, setRightSelectedKeys ] = useState<IdType[]>([])
+
+  const List = (list_props: IListProps) => {
+    const { data, selected_keys, setSelectedKeys } = list_props
+
+    const toggleSelect = (id: IdType) => {
+      setSelectedKeys(
+        selected_keys.includes(id) ?
+          selected_keys.filter(i => i != id) :
+          [ ...selected_keys, id ],
+      )
+    }
+
+    return (
+      <div className={styles.list}>
+        {data.map(item => {
+          const is_selected = selected_keys.includes(item.id)
+
+          return (
+            <Box
+              key={item.id}
+              className={clsx(styles.item, is_selected && styles.selected)}
+              px={3}
+              py={1}
+              onClick={() => toggleSelect(item.id)}
+            >
+              {
+                render ?
+                  render(item) :
+                  item.title || item.id
+              }
+            </Box>
+          )
+        })}
+      </div>
+    )
+  }
+
+  const moveLeftToRight = () => {
+    let result = [ ...right_keys, ...left_selectd_keys ]
+    setRightKeys(result)
+    setLeftSelectedKeys([])
+    onChange && onChange(result)
+  }
+
+  const moveRightToLeft = () => {
+    let result = right_keys.filter(i => !right_selectd_keys.includes(i))
+    setRightKeys(result)
+    setRightSelectedKeys([])
+    onChange && onChange(result)
+  }
+
+  return (
+    <div className={styles.root}>
+      <Grid templateColumns="minmax(0, 1fr) 40px minmax(0, 1fr)" gap={1}>
+        <Box borderWidth="1px" borderRadius="md">
+          <Box className={styles.title} borderBottomWidth={1} px={3} py={1}>
+            {lang.all} <span>({dataSource.length})</span>
+          </Box>
+          <List
+            data={dataSource.filter(i => !right_keys.includes(i.id))}
+            selected_keys={left_selectd_keys}
+            setSelectedKeys={setLeftSelectedKeys}
+          />
+        </Box>
+        <Center h="100%">
+          <VStack>
+            <IconButton
+              size="sm"
+              variant="outline"
+              aria-label="Move to right"
+              icon={<ArrowForwardIcon/>}
+              isDisabled={left_selectd_keys.length === 0}
+              onClick={moveLeftToRight}
+            />
+            <IconButton
+              size="sm"
+              variant="outline"
+              aria-label="Move to left"
+              icon={<ArrowBackIcon/>}
+              isDisabled={right_selectd_keys.length === 0}
+              onClick={moveRightToLeft}
+            />
+          </VStack>
+        </Center>
+        <Box borderWidth="1px" borderRadius="md">
+          <Box className={styles.title} borderBottomWidth={1} px={3} py={1}>
+            {lang.selected} <span>({right_keys.length})</span>
+          </Box>
+          <List
+            data={dataSource.filter(i => right_keys.includes(i.id))}
+            selected_keys={right_selectd_keys}
+            setSelectedKeys={setRightSelectedKeys}
+          />
+        </Box>
+      </Grid>
+    </div>
+  )
+}
+
+export default Transfer
