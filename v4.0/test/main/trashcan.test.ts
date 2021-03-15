fd3b6dfdf36c1a835456f3672ec818f8f17eb89e
oldj <oldj.wu@gmail.com>
2021-03-15 08:33:22 +0800

    add test cases.

--- v4.0/test/main/trashcan.test.ts
+++ v4.0/test/main/trashcan.test.ts
@@ -6,7 +6,9 @@
 
 import assert = require('assert')
 import { clearData } from '@_t/_base'
+import { IHostsContentObject } from '@root/common/data'
 import {
+  clearTrashcan,
   deleteItemFromTrashcan,
   getBasicData,
   getHostsContent,
@@ -17,6 +19,7 @@ import {
   setHostsContent,
   setList,
 } from '@root/main/actions'
+import { swhdb } from '@root/main/data'
 
 describe('trashcan test', () => {
   beforeEach(async () => {
@@ -123,5 +126,124 @@ describe('trashcan test', () => {
     assert(list[1].children[2].children[0].id === '3.3.2')
     assert(list[1].children[2].children[1].id === '3.3.3')
     assert(list[1].children[2].children[2].id === '3.3.1')
+  })
+
+  it('folder delete test', async () => {
+    await setList([
+      { id: '1' },
+      { id: '2' },
+      {
+        id: '3', type: 'folder', children: [
+          { id: '3.1' },
+          { id: '3.2' },
+          {
+            id: '3.3', type: 'folder', children: [
+              { id: '3.3.1' },
+              { id: '3.3.2' },
+              { id: '3.3.3' },
+            ],
+          },
+          { id: '3.4' },
+        ],
+      },
+      { id: '4' },
+    ])
+
+    let hs: IHostsContentObject[] = await swhdb.collection.hosts.all()
+    assert(hs.length === 0)
+
+    await setHostsContent('1', '# 1')
+    await setHostsContent('2', '# 2')
+    await setHostsContent('3', '# 3')
+    await setHostsContent('3.1', '# 3.1')
+    await setHostsContent('3.2', '# 3.2')
+    await setHostsContent('3.3', '# 3.3')
+    await setHostsContent('3.3.1', '# 3.3.1')
+    await setHostsContent('3.3.2', '# 3.3.2')
+    await setHostsContent('3.3.3', '# 3.3.3')
+    await setHostsContent('3.4', '# 3.4')
+    await setHostsContent('4', '# 4')
+
+    let list = await getList()
+    assert(list.length === 4)
+    let tlist = await getTrashcanList()
+    assert(tlist.length === 0)
+
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 11)
+    assert(hs[0].content === '# 1')
+
+    await moveToTrashcan('3.2')
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 11)
+    await deleteItemFromTrashcan('3.2')
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 10)
+
+    await moveToTrashcan('3')
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 10)
+    await deleteItemFromTrashcan('3')
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 3)
+  })
+
+  it('clear test', async () => {
+    await setList([
+      { id: '1' },
+      { id: '2' },
+      {
+        id: '3', type: 'folder', children: [
+          { id: '3.1' },
+          { id: '3.2' },
+          {
+            id: '3.3', type: 'folder', children: [
+              { id: '3.3.1' },
+              { id: '3.3.2' },
+              { id: '3.3.3' },
+            ],
+          },
+          { id: '3.4' },
+        ],
+      },
+      { id: '4' },
+    ])
+
+    let hs: IHostsContentObject[] = await swhdb.collection.hosts.all()
+    assert(hs.length === 0)
+
+    await setHostsContent('1', '# 1')
+    await setHostsContent('2', '# 2')
+    await setHostsContent('3', '# 3')
+    await setHostsContent('3.1', '# 3.1')
+    await setHostsContent('3.2', '# 3.2')
+    await setHostsContent('3.3', '# 3.3')
+    await setHostsContent('3.3.1', '# 3.3.1')
+    await setHostsContent('3.3.2', '# 3.3.2')
+    await setHostsContent('3.3.3', '# 3.3.3')
+    await setHostsContent('3.4', '# 3.4')
+    await setHostsContent('4', '# 4')
+
+    let list = await getList()
+    assert(list.length === 4)
+    let tlist = await getTrashcanList()
+    assert(tlist.length === 0)
+
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 11)
+
+    await moveToTrashcan('1')
+    await moveToTrashcan('2')
+    await moveToTrashcan('3')
+    await moveToTrashcan('4')
+
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 11)
+
+    await clearTrashcan()
+    hs = await swhdb.collection.hosts.all()
+    assert(hs.length === 0)
+    assert((await getList()).length === 0)
+    assert((await getTrashcanList()).length === 0)
   })
 })
