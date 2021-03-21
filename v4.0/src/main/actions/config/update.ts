f68c1e2c945161c9514e004795dd8ba8e275f9b0
oldj <oldj.wu@gmail.com>
2021-03-21 19:12:22 +0800

    update and show tray title.

--- v4.0/src/main/actions/config/update.ts
+++ v4.0/src/main/actions/config/update.ts
@@ -3,9 +3,12 @@
  * @homepage: https://oldj.net
  */
 
+import { updateTrayTitle } from '@main/actions'
 import { cfgdb } from '@main/data'
 import { ConfigsType } from '@root/common/default_configs'
 
 export default async (data: Partial<ConfigsType>) => {
   await cfgdb.dict.cfg.update(data)
+
+  await updateTrayTitle(!!data.show_title_on_tray)
 }
