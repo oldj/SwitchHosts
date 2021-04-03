2e950398720924e065cde925da03b6d8e1e6d20c
oldj <oldj.wu@gmail.com>
2021-04-04 00:43:21 +0800

    added the ability to import data of v3.

--- src/main/actions/import.ts
+++ src/main/actions/import.ts
@@ -4,6 +4,7 @@
  * @homepage: https://oldj.net
  */
 
+import importV3Data from '@main/actions/migrate/importV3Data'
 import getI18N from '@main/core/getI18N'
 import { swhdb } from '@main/data'
 import { dialog } from 'electron'
@@ -40,16 +41,29 @@ export default async (): Promise<boolean | string> => {
     return 'parse_error'
   }
 
-  if (typeof data !== 'object' || !data.version || !Array.isArray(data.version) || !data.data) {
+  if (typeof data !== 'object' || !data.version || !Array.isArray(data.version)) {
     return 'invalid_data'
   }
 
   let { version } = data
   if (version[0] === 3) {
     // import v3 data
+    try {
+      await importV3Data(data)
+    } catch (e) {
+      console.error(e)
+      return 'invalid_v3_data'
+    }
+
+    return true
   }
+
   if (version[0] > 4) {
     return 'new_version'
+  }
+
+  if (!data.data || typeof data.data !== 'object') {
+    return 'invalid_data_key'
   }
 
   await swhdb.loadJSON(data.data)
