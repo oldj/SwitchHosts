ce4482440ec2e61264c3aaae18fb87b5219ac8a1
oldj <oldj.wu@gmail.com>
2023-04-15 11:26:33 +0800

    Update dependencies.

--- src/main/actions/checkUpdate.ts
+++ src/main/actions/checkUpdate.ts
@@ -5,28 +5,13 @@
  */
 
 import { broadcast } from '@main/core/agent'
-import { localdb } from '@main/data'
-import { GET } from '@main/libs/request'
-import { server_url } from '@common/constants'
 import events from '@common/events'
 import version from '@/version.json'
 import { compareVersions } from 'compare-versions'
-import { v4 as uuid4 } from 'uuid'
 import * as updater from '@main/core/updater'
-import type { AxiosResponse } from 'axios'
-
-const getUniqueId = async (): Promise<string> => {
-  let uid: string | undefined = await localdb.dict.local.get('uid')
-  if (!uid) {
-    uid = uuid4()
-    await localdb.dict.local.set('uid', uid)
-  }
-  return uid
-}
 
 export default async (): Promise<boolean | null> => {
   let server_version = await updater.checkUpdate()
-  console.log(1111, server_version)
   if (!server_version) {
     return false
   }
--- vite.render.config.ts
+++ vite.render.config.ts
@@ -46,6 +46,7 @@ export default defineConfig({
     },
   },
   server: {
+    host: '127.0.0.1',
     port: 8220,
   },
 })
